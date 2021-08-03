c0379f1ecb044515a86c8117442fb637fcea89b4
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-08-03 17:27:34 +0200

    calcoli in js

--- myscript.js
+++ myscript.js
@@ -1,5 +1,7 @@
 console.log("Prova");
 
+var eta = 20;
+var eta_silente = 500;
 document.getElementById("start").innerHTML = "start bootstrap now fidate";
 console.log("ho cambiato l'h2 nell'header");
 
@@ -7,3 +9,4 @@ document.getElementsByClassName("nav_a").innerHTML = "schiaccia qui";
 console.log("tutto apposto");
 
 console.log(document.getElementById("start").innerHTML + " " +  document.getElementsByClassName("nav_a").innerHTML);
+console.log("ho " + eta + " anni, tra " + (eta_silente - eta) + " avrò l'attuale età di silente")
