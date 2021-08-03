2c147e9772210fcf4bf2882df5e2a9bd997c64ba
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-08-03 17:22:37 +0200

    refactoring

--- myscript.js
+++ myscript.js
@@ -1,1 +1,9 @@
 console.log("Prova");
+
+document.getElementById("start").innerHTML = "start bootstrap now fidate";
+console.log("ho cambiato l'h2 nell'header");
+
+document.getElementsByClassName("nav_a").innerHTML = "schiaccia qui";
+console.log("tutto apposto");
+
+console.log(document.getElementById("start").innerHTML + " " +  document.getElementsByClassName("nav_a").innerHTML);
